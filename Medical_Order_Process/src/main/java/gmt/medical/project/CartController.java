62ffca9930cbadeaa332adac1d645c2f7701d283
greengit2 <feelsun12345@gmail.com>
2023-06-01 14:28:20 +0900

    Carthtml/css완료/CartController/MarshLogo추가

--- Medical_Order_Process/src/main/java/gmt/medical/project/CartController.java
+++ Medical_Order_Process/src/main/java/gmt/medical/project/CartController.java
@@ -4,14 +4,11 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
-
 @Controller
-public class HomeController {
-	@RequestMapping(value = "/", method = RequestMethod.GET)
-	public String home() {
-		return "Home";
+public class CartController {
+
+	@RequestMapping(value = "/Cart", method = RequestMethod.GET)
+	public String cart() {
+		return "Cart";
 	}
-	
-	
-	
 }
