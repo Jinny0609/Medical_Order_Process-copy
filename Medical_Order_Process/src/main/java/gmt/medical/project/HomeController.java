6d136f83d3cb6c832aa552478c854cc0e5f9919e
Kim-Ju-Yoon <kj5841@naver.com>
2023-06-12 14:42:52 +0900

    컨트롤러 수정

--- Medical_Order_Process/src/main/java/gmt/medical/project/HomeController.java
+++ Medical_Order_Process/src/main/java/gmt/medical/project/HomeController.java
@@ -1,12 +1,8 @@
 package gmt.medical.project;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-
-import gmt.medical.model.Shipping_address;
-import gmt.medical.service.Shipping_address_Service;
 
 
 @Controller
@@ -65,13 +61,4 @@ public class HomeController {
 		return "Checkout";
 	}
 	
-	@RequestMapping(value = "/saveAddress", method = RequestMethod.GET)
-	public String saveAddress(Shipping_address address) {
-		addressService.saveAddress(address);
-		System.out.println(address);
-		return "redirect:/Shipping_address_List";
-	}
-	
-	@Autowired
-    private Shipping_address_Service addressService;
 }
---
+++ Medical_Order_Process/src/main/java/gmt/medical/project/InfoController.java
@@ -0,0 +1,22 @@
+package gmt.medical.project;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+import gmt.medical.model.Shipping_address;
+import gmt.medical.service.Shipping_address_Service;
+
+@Controller
+public class InfoController {
+	@RequestMapping(value = "/saveAddress", method = RequestMethod.GET)
+	public String saveAddress(Shipping_address address) {
+		addressService.saveAddress(address);
+		System.out.println(address);
+		return "redirect:/Shipping_address_List";
+	}
+	
+	@Autowired
+    private Shipping_address_Service addressService;
+}
