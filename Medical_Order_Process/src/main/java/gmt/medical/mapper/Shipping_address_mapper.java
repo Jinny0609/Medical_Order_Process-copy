2fc975f38f6bb11733eab9db0ac418e610c507c5
Kim-Ju-Yoon <kj5841@naver.com>
2023-06-15 10:13:36 +0900

    게시판 수정 삭제 오류 해결 매퍼에서 파람으로 구분해줘야됨

--- Medical_Order_Process/src/main/java/gmt/medical/mapper/Shipping_address_mapper.java
+++ Medical_Order_Process/src/main/java/gmt/medical/mapper/Shipping_address_mapper.java
@@ -2,6 +2,8 @@ package gmt.medical.mapper;
 
 import java.util.List;
 
+import org.apache.ibatis.annotations.Param;
+
 import gmt.medical.model.Shipping_address;
 
 public interface Shipping_address_mapper {
@@ -10,9 +12,9 @@ public interface Shipping_address_mapper {
 	// 배송지 리스트
 	public List<Shipping_address> getAllAddresses(int user_id);
 	// 배송지 수정 데이터 가져오기
-	public Shipping_address getAddressById(int address_id);
+	public Shipping_address getAddressById(@Param("address_id") int address_id, @Param("user_id")int user_id);
 	// 배송지 정보 수정 후 저장
 	public void updateAddress(Shipping_address address);
 	// 배송지 정보 삭제
-	public void deleteAddress(int address_id,int user_id);
+	public void deleteAddress(@Param("address_id") int address_id, @Param("user_id")int user_id);
 }
