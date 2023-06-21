052bdd3b4a9ec47e38f37f3f02811bba964b0102
greengit2 <feelsun12345@gmail.com>
2023-06-21 13:43:36 +0900

    충돌 오류해결

--- Medical_Order_Process/src/main/java/gmt/medical/model/Product_info.java
+++ Medical_Order_Process/src/main/java/gmt/medical/model/Product_info.java
@@ -2,6 +2,8 @@ package gmt.medical.model;
 
 import java.util.List;
 
+import org.springframework.web.multipart.MultipartFile;
+
 public class Product_info {
 	private int product_id;
     private String product_image;
@@ -10,23 +12,8 @@ public class Product_info {
     private int product_count;
     private String image_path;
     private int category_id;
-	private String cart_option;
+    private MultipartFile save_image;
     private List<String> optionNames;
-    
-    @Override
-	public String toString() {
-	    return "Product_info : {" +
-	            "product_id=" + product_id +
-	            "cart_option=" + cart_option +
-	            ", product_image='" + product_image + '\'' +
-	            ", product_name='" + product_name + '\'' +
-	            ", product_price=" + product_price +
-	            ", product_count=" + product_count +
-	            ", image_path='" + image_path + '\'' +
-	            ", category_id=" + category_id +
-	            ", optionNames=" + optionNames +
-	            '}';
-	}
     
 	public int getProduct_id() {
 		return product_id;
@@ -70,16 +57,32 @@ public class Product_info {
 	public void setCategory_id(int category_id) {
 		this.category_id = category_id;
 	}
+	public MultipartFile getSave_image() {
+		return save_image;
+	}
+	public void setSave_image(MultipartFile save_image) {
+		this.save_image = save_image;
+	}
 	public List<String> getOptionNames() {
 		return optionNames;
 	}
 	public void setOptionNames(List<String> optionNames) {
 		this.optionNames = optionNames;
 	}
-	public String getCart_option() {
-		return cart_option;
-	}
-	public void setCart_option(String cart_option) {
-		this.cart_option = cart_option;
-	}
+	   
+    @Override
+    public String toString() {
+        return "{" +
+                "product_id=" + product_id +
+                ", product_image='" + product_image + '\'' +
+                ", product_name='" + product_name + '\'' +
+                ", product_price=" + product_price +
+                ", product_count=" + product_count +
+                ", image_path='" + image_path + '\'' +
+                ", category_id=" + category_id +
+                ", save_image='" + (save_image != null ? save_image.getOriginalFilename() : "null") + '\'' +
+                ", optionNames=" + optionNames +
+                '}';
+    }
+	
 }
